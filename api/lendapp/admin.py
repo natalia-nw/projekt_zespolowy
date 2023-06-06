b7929ca5b63f1108cc5b00aae009ec3b8ede2ac8
Artur Zalecki <77029204+Arti2802@users.noreply.github.com>
2023-06-06 11:02:36 +0200

    delete api

--- api/lendapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Item, Deal
-from reversion.admin import VersionAdmin
-
-
-@admin.register(User, Item, Deal)
-class MyModelAdmin(VersionAdmin):
-    pass
